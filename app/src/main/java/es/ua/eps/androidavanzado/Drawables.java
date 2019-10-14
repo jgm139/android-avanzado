51e14cae9542862f9790e5f16af423648ba4c135
Julia García Martínez <jgm139@alu.ua.es>
2019-10-14 18:24:29 +0200

    Ejercicio 1. Personalización del aspecto

---
+++ app/src/main/java/es/ua/eps/androidavanzado/Drawables.java
@@ -0,0 +1,14 @@
+package es.ua.eps.androidavanzado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Drawables extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawables);
+    }
+}
--- app/src/main/java/es/ua/eps/androidavanzado/MainActivity.java
+++ app/src/main/java/es/ua/eps/androidavanzado/MainActivity.java
@@ -2,13 +2,28 @@ package es.ua.eps.androidavanzado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button_drawables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_drawables = findViewById(R.id.button_drawables);
+
+        button_drawables.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Drawables.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
