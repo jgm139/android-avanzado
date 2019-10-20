70a71e5141ad217043578435b7f928f25ecad70e
Julia García Martínez <jgm139@alu.ua.es>
2019-10-20 17:36:35 +0200

    Estilos y temas: ejercicio propuesto

--- app/src/main/java/es/ua/eps/androidavanzado/MainActivity.java
+++ app/src/main/java/es/ua/eps/androidavanzado/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button_drawables;
+    private Button button_styles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button_drawables = findViewById(R.id.button_drawables);
+        button_styles = findViewById(R.id.button_styles);
 
         button_drawables.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Drawables.class);
+                startActivity(intent);
+            }
+        });
+
+        button_styles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, sPrincipal.class);
                 startActivity(intent);
             }
         });
